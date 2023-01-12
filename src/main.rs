168127f33508e843d3d33231c9ff1805d88f5e84
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-12 15:52:12 -0800

    random vector function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// suse std::collections::HashMap;
+
+// Either create a vector with a bunch of integers or write a function that can generate integers then push to a vector
+
+// Write a function that can iterate over this vector and calculate the median
+
+// push this vector onto a hashmap, iterate over the hashmap with a function to calculate the mode
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let mut vec = Vec::new();
+
+    for _ in 0..100 {
+        vec.push(rng.gen_range(1..501));
+    }
+
+    println!("{:?}", vec);
 }
+
+
+
