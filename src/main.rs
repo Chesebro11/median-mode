cf96e839fc160cc5cad4672028c1b70a475835c8
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-13 16:48:57 -0800

    cleaned up comments, commiting to 3 functions fuck it

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,36 @@
-//After figuring out how to improve calculate mode to actually calculate the full mode
-// this code could be refactored, I think two individual functions to calculate median and mode
-// is excessive
+// rand::Rng; for generating the vector of integers
 use rand::Rng;
+
+// Hashmap is used for calculating the mode
 use std::collections::HashMap;
 
 fn main() {
     let mut rng = rand::thread_rng();
     let mut vec = Vec::new();
+
     // generates 100 different integers between 1 and 500
     for _ in 0..100 {
         vec.push(rng.gen_range(1..501));
     }
+
     // prints out the vector
     println!("{:?}", vec);
+
     // passes the vector to the calculate_median function
     let median = calculate_median(&vec);
+
     // prints the median
     println!("the median of the vector is: {}", median);
+
     // passes the vector to calculate_mode function
     let mode = calculate_mode(vec);
+    
     // prints the mode
     println!("The mode of the vector is: {:?}", mode);
-
-    // let (median, mode) = calculate_median_mode(&mut vec);
-    // println!("The mode of the vector is: {}, and the Median of the vector is {}", mode, median)
 }
 
 // function that borrows the vector and calculates the median
+
 fn calculate_median(vec: &Vec<i32>) -> f64 {
     let mut vec = vec.clone();
     vec.sort();
@@ -41,8 +45,11 @@ fn calculate_median(vec: &Vec<i32>) -> f64 {
     }
 }
 
-// this calculates the mode.. sort of. It doesn't keep track of all of the most occuring integers
-// This should be written to be able to keep track if theres 1, 2, 3... etc. numbers occurring the most
+//This now properly calculates the mode (checks for multiple possible solutions)
+// Taking the vector generated in the main function iterate over it through a hashmap
+// count each integer in the vector, whatever is > than the max_count gets pushed  onto the vector
+// if something surpasses the count clear the vector and push the new integer onto it
+// if something is = to the count just push to the vector
 fn calculate_mode(vec: Vec<i32>) -> Vec<i32> {
     let mut map = HashMap::new();
     let mut mode = Vec::new();
@@ -62,36 +69,4 @@ fn calculate_mode(vec: Vec<i32>) -> Vec<i32> {
         }
         mode
     }
-    
-// trying to figure out how to get this all into one function but struggling with mismatched types
-// When I've solved the mismatched types issue i then run into an issue with returning the mode
-// where I receive some sort of error about referencing / dereferncing a value
-// fn calculate_median_mode(vec: &mut Vec<i32>) -> (f64, Vec<i32>) {
-//     vec.sort();
-//     let len = vec.len();
 
-//     let mut map = HashMap::new();
-//     let mut median = 0.0;
-//     let mut mode = Vec::new();
-//     let mut max_count = 0;
-
-//     for i in vec {
-//         let count = map.entry(i).or_insert(0);
-//         *count += 1;
-
-    //     if *count > max_count {
-    //         max_count = *count;
-    //         mode.clear();
-    //         mode.push(i);
-    //     } else if *count == max_count {
-    //         mode.push(i);
-    //     }
-    // }
-    // if len % 2 == 0 {
-    //     let mid = len / 2;
-    //     median = (vec[mid] as f64 + vec[mid - 1] as f64) / 2.0;
-    // } else {
-    //     median = vec[len / 2] as f64;
-    // }
-//     (median, mode)
-// }
